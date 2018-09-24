5c0710e35388cfbdbdf50f1ffa9ee442b888a2a9
Nick Dean <klick5000@gmail.com>
2018-09-23 22:16:56 -0600

    Turned answerChoices into button lookalikes

--- main.js
+++ main.js
@@ -44,10 +44,12 @@ function generateQuestion() {
 
   for (let i = 0; i < currentQuestionObj.choices.length; i++) {
     $('.question-choices').append(`
-      <label class="answerOption">
-        <input class="answerRadio" type="radio" name="answer" value="${currentQuestionObj.choices[i]}" required>
-        <span>${currentQuestionObj.choices[i]}</span>
-      </label>
+      <div class="answerOption">  
+        <label>
+          <input class="answerRadio" type="radio" name="answer" value="${currentQuestionObj.choices[i]}" required>
+          <span>${currentQuestionObj.choices[i]}</span>
+        </label>
+      </div>
   `);
   }
 
